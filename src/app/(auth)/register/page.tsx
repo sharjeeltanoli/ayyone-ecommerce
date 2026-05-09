4ae6f442b008d8381abf3ff592a9ef3605be68b0
sharjeeltanoli <sharjeeel3@gmail.com>
2026-05-09 11:20:40 +0500

    auth pages

---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import LoginPage from '../login/page';
+
+const RegisterPage = () => {
+  return (
+    <div>
+      <h1>Register Page</h1>
+      {/* Aapka register form yahan aayega */}
+    </div>
+  )
+}
+
+export default RegisterPage
